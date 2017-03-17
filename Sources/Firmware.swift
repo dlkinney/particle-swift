371f66ef598171483ba17e7749c5e91afe80602a
vakoc <mark@vakoc.com>
2017-03-17 10:22:24 -0600

    Parse and expose the binary size info details from a success firmware compilation

--- Sources/Firmware.swift
+++ Sources/Firmware.swift
@@ -32,6 +32,49 @@ public struct SourceFile {
 /// Detail of a binary produced by a successfull compilation
 public struct BinaryInfo {
     
+    /// Details of the size of the binary produced during compilation
+    public struct Size {
+    
+        /// The text, or compiled code, segment size
+        public let text: Int
+    
+        /// The data, or initialized variables, segment size
+        public let data: Int
+        
+        /// The bss, or uninitialized variables, segment size
+        public let bss: Int
+        
+        /// The total size, in bytes
+        public let size: Int
+        
+        /// Create the size information from a string
+        ///
+        /// The incoming string should look like:
+        ///
+        ///      "   text\t   data\t    bss\t    dec\t    hex\tfilename\n 101312\t   2152\t   9880\t 113344\t  1bac0\t"
+        ///
+        /// Returns nil if the supplied string could not be parsed
+        internal init?(_ string: String) {
+            
+            let lines = string.components(separatedBy: .newlines)
+            if lines.count < 2 {
+                return nil
+            }
+            
+            let vals = lines[1].components(separatedBy: .whitespaces).filter { !$0.isEmpty }
+            if vals.count < 4 {
+                return nil
+            }
+            
+            guard let text = Int(vals[0]), let data = Int(vals[1]), let bss = Int(vals[2]), let size = Int(vals[3]) else { return nil }
+            
+            self.text = text
+            self.data = data
+            self.bss = bss
+            self.size = size
+        }
+    }
+    
     /// The unique identifier of the binary
     public let binaryId: String
     
@@ -42,9 +85,25 @@ public struct BinaryInfo {
     public let expires: Date
     
     /// Information about the size of the binary
-    public let sizeInfo: String
+    public let sizeInfo: Size
     
 }
+
+extension BinaryInfo.Size: Equatable {
+    
+    public static func ==(lhs: BinaryInfo.Size, rhs: BinaryInfo.Size) -> Bool {
+        return lhs.text == rhs.text && lhs.data == rhs.data && lhs.bss == rhs.bss && lhs.size == rhs.size
+    }
+    
+}
+
+extension BinaryInfo.Size: Comparable {
+    
+    public static func <(lhs: BinaryInfo.Size, rhs: BinaryInfo.Size) -> Bool {
+        return lhs.size < rhs.size
+    }
+}
+
 
 /// The result of a compilation web service invocation. 
 ///
@@ -70,7 +129,7 @@ extension ParticleCloud {
     /// the compilation was successful
     ///
     /// - Parameters:
-    ///   - files: The fiels to compile
+    ///   - files: The files to compile
     ///   - deviceID: The identifier of the device to target
     ///   - product: The product to build for
     ///   - build_target_version: The firmware version to compile against. nil defaults to latest
@@ -139,14 +198,14 @@ extension ParticleCloud {
                                 let binary_url = j["binary_url"] as? String,
                                 let expires_at = j["expires_at"] as? String,
                                 let expires = expires_at.dateWithISO8601String,
-                                let size_info = j["sizeInfo"] as? String else {
+                                let size_info = j["sizeInfo"] as? String, let si = BinaryInfo.Size(size_info) else {
                                     
                                     let message = String(data: data, encoding: String.Encoding.utf8) ?? ""
                                     warn("failed to receive expected compile result with response: \(String(describing: response)) and message body \(message)")
                                     return completion(.failure(ParticleError.compileRequestFailed(message)))
                             }
                             
-                            let buildResult = BinaryInfo(binaryId: binary_id, binaryUrl: binary_url, expires: expires, sizeInfo: size_info)
+                            let buildResult = BinaryInfo(binaryId: binary_id, binaryUrl: binary_url, expires: expires, sizeInfo: si)
                             completion(.success(.compileSuccess(buildResult)))
                         } else {
                             let result: BuildResult = .compileFailure(output: j["output"] as? String ?? "", stdout: j["stdout"] as? String ?? "", errors: j["errors"] as? [String] ?? [])
